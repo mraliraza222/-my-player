a1ba38846a6c90d2553976bb2d53f75988636f75
mraliraza222 <alirazaashiq64@gmail.com>
2026-05-09 11:53:04 +0500

    Add Colors and FontSizes constants in Theme.js

---
+++ Theme.js
@@ -0,0 +1,15 @@
+export const Colors = {
+  background: "#000000",
+  primary: "#1e90ff",
+  text: "#ffffff",
+  secondaryText: "#aaaaaa",
+  card: "#111111",
+  danger: "#ff4444",
+};
+
+export const FontSizes = {
+  small: 14,
+  medium: 18,
+  large: 24,
+  xlarge: 32,
+};
